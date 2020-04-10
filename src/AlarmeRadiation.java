bbbd936d8020e19c65e16e27eef4e5970b73092b
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-04-10 14:58:45 +0200

    Update AlarmeRadiation.java

--- src/AlarmeRadiation.java
+++ src/AlarmeRadiation.java
@@ -1,42 +1,22 @@
-import java.util.GregorianCalendar;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class AlarmeRadiation {
-	
-	private GregorianCalendar date;
-	private String localisation; 
-	private int importance;
-	private int niveau;
-	
-	public AlarmeRadiation(GregorianCalendar date, String localisation, int importance, int niveau) {
-		super();
-		if(importance>3 || importance<1 || niveau>100 || niveau<1) {
-			System.out.println("Erreur de saisie !");
-		}
-		else {
-			this.date = date;
-			this.localisation = localisation;
-			this.importance = importance;
-			this.niveau = niveau;
-		}
-	}
 
-	public GregorianCalendar getDate() {
-		return date;
-	}
-
-	public String getLocalisation() {
-		return localisation;
-	}
-
-	public int getImportance() {
-		return importance;
-	}
-
-	public int getNiveau() {
-		return niveau;
-	}
+public class AlarmeRadiation implements AlerteRadiationListener{
 	
+	public static ArrayList<String> listeGaz = new ArrayList<String>();
 	
-	
+	public static void main(String[] args) {
+	    listeGaz.add("hydrogène");
+	    listeGaz.add("hélium");
+	    listeGaz.add("CO2");
+	    listeGaz.add("Autre");
+	  }
+
+	@Override
+	public void receptionRadiation(Date dateApparition, String localisation, int niveau, int importance) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
