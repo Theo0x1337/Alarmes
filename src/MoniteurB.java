f25ffbbfe73b45a533c4cc84637204d39684bd65
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-04-10 14:31:08 +0200

    Create MoniteurB.java

---
+++ src/MoniteurB.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class MoniteurB {
+	
+	static String type = "B";
+	ArrayList<AlarmeGaz> listeAlarmeGaz = new ArrayList<AlarmeGaz>();
+	ArrayList<AlarmeRadiation> listeAlarmeRadiation = new ArrayList<AlarmeRadiation>();
+	
+	public MoniteurB(ArrayList<AlarmeGaz> listeAlarmeGaz, ArrayList<AlarmeRadiation> listeAlarmeRadiation) {
+		super();
+		this.listeAlarmeGaz = listeAlarmeGaz;
+		this.listeAlarmeRadiation = listeAlarmeRadiation;
+	}
+
+	public MoniteurB() {
+		super();
+	}
+
+	public ArrayList<AlarmeGaz> getListeAlarmeGaz() {
+		return listeAlarmeGaz;
+	}
+
+	public void setListeAlarmeGaz(ArrayList<AlarmeGaz> listeAlarmeGaz) {
+		this.listeAlarmeGaz = listeAlarmeGaz;
+	}
+
+	public ArrayList<AlarmeRadiation> getListeAlarmeRadiation() {
+		return listeAlarmeRadiation;
+	}
+
+	public void setListeAlarmeRadiation(ArrayList<AlarmeRadiation> listeAlarmeRadiation) {
+		this.listeAlarmeRadiation = listeAlarmeRadiation;
+	}
+
+	public static String getType() {
+		return type;
+	}
+	
+}
