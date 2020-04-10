4d13b044424a4adaba7a26d5ca64e7e6ba547383
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-04-10 14:20:37 +0200

    Update AlarmeIncendie.java

--- src/AlarmeIncendie.java
+++ src/AlarmeIncendie.java
@@ -1,3 +1,32 @@
-public class AlarmeIncendie {
+import java.util.GregorianCalendar;
 
+public class AlarmeIncendie {
+	
+	private GregorianCalendar date;
+	private String localisation; 
+	private int importance;
+	
+	public AlarmeIncendie(GregorianCalendar date, String localisation, int importance) {
+		super();
+		if(importance>3 || importance<1) {
+			System.out.println("Erreur de saisie !");
+		}
+		else {
+			this.date = date;
+			this.localisation = localisation;
+			this.importance = importance;
+		}
+	}
+
+	public GregorianCalendar getDate() {
+		return date;
+	}
+
+	public String getLocalisation() {
+		return localisation;
+	}
+
+	public int getImportance() {
+		return importance;
+	}
 }
