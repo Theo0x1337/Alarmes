d90c78ab63eaf579aa76bf77f417c8c949a3d04c
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-04-10 14:20:39 +0200

    Update AlarmeRadiation.java

--- src/AlarmeRadiation.java
+++ src/AlarmeRadiation.java
@@ -1,3 +1,42 @@
-public class AlarmeRadiation {
+import java.util.GregorianCalendar;
 
+public class AlarmeRadiation {
+	
+	private GregorianCalendar date;
+	private String localisation; 
+	private int importance;
+	private int niveau;
+	
+	public AlarmeRadiation(GregorianCalendar date, String localisation, int importance, int niveau) {
+		super();
+		if(importance>3 || importance<1 || niveau>100 || niveau<1) {
+			System.out.println("Erreur de saisie !");
+		}
+		else {
+			this.date = date;
+			this.localisation = localisation;
+			this.importance = importance;
+			this.niveau = niveau;
+		}
+	}
+
+	public GregorianCalendar getDate() {
+		return date;
+	}
+
+	public String getLocalisation() {
+		return localisation;
+	}
+
+	public int getImportance() {
+		return importance;
+	}
+
+	public int getNiveau() {
+		return niveau;
+	}
+	
+	
+	
+	
 }
