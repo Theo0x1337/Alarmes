3d09db39b554fdd135bec470cb8ece560538c340
theo bernardin <theo.bernardin@gmail.com>
2020-05-10 23:56:30 +0200

    classe mere des classes listeners de la menubar

---
+++ src/MenuItemListener.java
@@ -0,0 +1,23 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class MenuItemListener implements ActionListener {
+	
+	
+	protected Monitoring jfm;
+	
+	
+	public MenuItemListener(Monitoring jfm) {
+		this.jfm = jfm;
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
